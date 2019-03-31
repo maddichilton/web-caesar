a6f0c6b9961da1f45383273ad971d99fbe406c33
Maddi Chilton <maddichilton@gmail.com>
2019-03-30 20:38:27 -0500

    created flask app and created blank form

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <form action="/CHANGE" method="post">
+        <label for="rot">
+            Rotate by:
+            <input type="text" id="rot" name="rot" value="0" />
+        </label>
+        <textarea id="text" name="text"> </textarea>
+        <input type="submit" value="Submit"/>
+      </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
