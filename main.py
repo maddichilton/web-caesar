e248e21b6b430d0963ec4f24b1891969599cea61
Maddi Chilton <maddichilton@gmail.com>
2019-04-01 16:52:33 -0500

    put encrypted text in text box

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,7 +31,7 @@ form = """
             Rotate by:
             <input type="text" id="rot" name="rot" value="0" />
         </label>
-        <textarea id="text" name="text"> </textarea>
+        <textarea id="text" name="text">{0}</textarea>
         <input type="submit" value="Submit"/>
       </form>
     </body>
@@ -45,11 +45,11 @@ def encrypt():
     
     encrypted_text = rotate_string(text, rot)
 
-    return "<h1>" + encrypted_text + "</h1>"
+    return form.format(encrypted_text)
 
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 app.run()
